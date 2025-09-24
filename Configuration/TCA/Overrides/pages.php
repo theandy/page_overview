22e231afa51ed39d4a8f476e003d175aaf1d228c
andreasloewer <info@andreasloewer.de>
2025-09-24 09:59:55 +0200

    no message

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,6 +2,39 @@
 
 defined('TYPO3') or die('Access denied.');
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+// Neue Felder definieren
+$additionalColumns = [
+    'tx_page_image' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:page_images/Resources/Private/Language/locallang_db.xlf:pages.tx_page_image',
+        'config' => ExtensionManagementUtility::getFileFieldTCAConfig(
+            'tx_page_overview_img',
+            [
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'Bild hinzufügen für die Ansicht',
+                    'showPossibleLocalizationRecords' => true,
+                ],
+                'maxitems' => 1,
+            ],
+            'jpg,jpeg,png,svg,gif' // Erlaubte Bildtypen
+        ),
+    ],
+];
+
+// Felder registrieren
+ExtensionManagementUtility::addTCAcolumns('pages', $additionalColumns);
+
+// Felder dem Backend-Formular hinzufügen
+ExtensionManagementUtility::addToAllTCAtypes(
+    'pages',
+    'tx_page_overview_img',
+    '1',
+    'after:title'
+);
+
 call_user_func(function()
 {
     /**
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,6 @@
 #
 # Add SQL definition of database tables
 #
+CREATE TABLE pages (
+    tx_page_overview_img int(11) unsigned DEFAULT '0' NOT NULL
+);
