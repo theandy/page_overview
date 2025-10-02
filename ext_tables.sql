0276484131a71312417eeb645fc1b5eb19d162b2
andreasloewer <info@andreasloewer.de>
2025-10-02 10:23:27 +0200

    no message

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,9 @@
 #
 CREATE TABLE pages (
     tx_page_overview_img int(11) unsigned DEFAULT '0' NOT NULL,
-    tx_page_overview_desc text,
+    tx_page_overview_desc text
+);
+
+CREATE TABLE tt_content (
     tx_page_overview_root int(11) DEFAULT '0' NOT NULL
 );
