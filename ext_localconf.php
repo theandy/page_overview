eaa1077559286eec50a717b4454fc105ed94efad
andreasloewer <info@andreasloewer.de>
2025-10-02 11:11:57 +0200

    no message

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,23 @@
 <?php
-
 defined('TYPO3') or die('Access denied.');
 
-/***************
- * Add default RTE configuration
+/**
+ * RTE Preset
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['page_overview'] = 'EXT:page_overview/Configuration/RTE/Default.yaml';
 
-/***************
- * PageTS
+/**
+ * PageTS einbinden
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:page_overview/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:page_overview/Configuration/TsConfig/Page/All.tsconfig">'
+);
+
+/**
+ * Stellt zusätzliche Seitenfelder im FE bereit, damit MenuProcessor sie liefert.
+ * Wichtig für: tx_page_overview_exclude, tx_page_overview_desc
+ */
+$fieldsToAdd = ['tx_page_overview_exclude', 'tx_page_overview_desc'];
+$existing = array_filter(array_map('trim', explode(',', (string)($GLOBALS['TYPO3_CONF_VARS']['FE']['additionalGetPageFields'] ?? ''))));
+$merged = array_unique(array_filter(array_merge($existing, $fieldsToAdd)));
+$GLOBALS['TYPO3_CONF_VARS']['FE']['additionalGetPageFields'] = implode(',', $merged);
