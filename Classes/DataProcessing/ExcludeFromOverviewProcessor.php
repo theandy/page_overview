d092bb70c93e94de0e2e473495453f5e142060d5
andreasloewer <info@andreasloewer.de>
2025-10-02 11:05:34 +0200

    no message

---
+++ Classes/DataProcessing/ExcludeFromOverviewProcessor.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Vendor\PageOverview\DataProcessing;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\DataProcessing\DataProcessorInterface;
+
+final class ExcludeFromOverviewProcessor implements DataProcessorInterface
+{
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ) {
+        $source = $processorConfiguration['source'] ?? 'menu';
+        $field  = $processorConfiguration['field']  ?? 'tx_page_overview_exclude';
+
+        if (!isset($processedData[$source]) || !is_array($processedData[$source])) {
+            return $processedData;
+        }
+
+        $processedData[$source] = array_values(array_filter(
+            $processedData[$source],
+            static fn(array $item): bool => empty($item['data'][$field])
+        ));
+
+        return $processedData;
+    }
+}
