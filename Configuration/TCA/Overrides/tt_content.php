d96ebe08c9ef61d0ce64f4dcc63313049ea3d456
andreasloewer <info@andreasloewer.de>
2026-01-15 11:42:22 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,7 +8,7 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 ExtensionManagementUtility::addTCAcolumns('tt_content', [
     'tx_page_overview_root' => [
         'exclude' => 1,
-        'label' => 'LLL:EXT:page_overview/Resources/Private/Language/Form/locallang_tabs.xlf:exclude.label',
+        'label' => 'LLL:EXT:page_overview/Resources/Private/Language/Form/locallang_tabs.xlf:root_page.label',
         'config' => [
             'type' => 'group',
             'internal_type' => 'db',
