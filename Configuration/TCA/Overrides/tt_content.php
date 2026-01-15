b3730afb05cf9a166911d50e0fcc10b2adcaa7d2
andreasloewer <info@andreasloewer.de>
2026-01-15 12:37:37 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -55,7 +55,7 @@ ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
     [
-        'LLL:EXT:page_overview/Resources/Private/Language/Form/locallang_tabs.xlf:tt_content.pageoverview_pages.title',
+        'LLL:EXT:page_overview/Resources/Private/Language/locallang.xlf:plugin.pages.title',
         'pageoverview_pages',
         'content-text',
     ],
