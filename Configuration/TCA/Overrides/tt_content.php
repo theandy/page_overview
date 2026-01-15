1a9867e300b3e1acf01b87ab15992e503ca8a093
andreasloewer <info@andreasloewer.de>
2026-01-15 12:46:25 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -51,9 +51,7 @@ ExtensionManagementUtility::addTcaSelectItem(
 );
 */
 
-// Backend-Language-File explizit registrieren, ExtensionManagementUtility::addTcaSelectItem() lädt KEINE Sprachdateien!
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:core/Resources/Private/Language/locallang_ttc.xlf'][] =
-    'EXT:page_overview/Resources/Private/Language/locallang.xlf';
+
 
 ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
